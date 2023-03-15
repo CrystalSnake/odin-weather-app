cfdf4592f39c505d02ed03a31f24c8a43f7b0f29
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-03-15 21:45:42 +0400

    Plug in API and console log weather data

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('test');
+let city = 'Ulyanovsk';
+const APIkey = '89b7db360fddc03e875ecd0daeda6f46';
+
+async function getWeather() {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`,
+      { mode: 'cors' }
+    );
+    response.json().then(function (response) {
+      console.log(response);
+    });
+  } catch (error) {
+    console.log(`Oops`);
+  }
+}
+
+getWeather();
