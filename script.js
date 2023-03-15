37a4a816e1bee3374bb9d6d3fb41d4d9ccf00a16
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2023-03-15 22:07:33 +0400

    Get from the JSON only data for app

--- script.js
+++ script.js
@@ -7,9 +7,23 @@ async function getWeather() {
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`,
       { mode: 'cors' }
     );
-    response.json().then(function (response) {
-      console.log(response);
-    });
+    response
+      .json()
+      .then(function (response) {
+        //console.log(response);
+        const weather = {};
+        weather.description = response.weather[0].description;
+        weather.temperature = response.main.temp;
+        weather.feelsLike = response.main.feels_like;
+        weather.humidity = response.main.humidity;
+        weather.pressure = response.main.pressure;
+        weather.windSpeed = response.wind.speed;
+        weather.windDirection = response.wind.deg;
+        return weather;
+      })
+      .then(function (response) {
+        console.log(response);
+      });
   } catch (error) {
     console.log(`Oops`);
   }
